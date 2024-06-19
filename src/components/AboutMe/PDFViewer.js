98a00dabd369a46d5fe9881c8b60b39dfd1d2cd4
Angel Rafael Jimenez Guillermo <jimeneza14@hotmail.com>
2024-06-19 18:52:39 -0400

    Layout Update

--- src/components/AboutMe/PDFViewer.js
+++ src/components/AboutMe/PDFViewer.js
@@ -4,13 +4,12 @@ const PDFViewer = () => {
   return (
     <div>
       <h2>Curriculum Vitae</h2>
-      <iframe
+      <iframe      
         title="Curriculum Vitae"
         width="100%"
         height="450px"
         src="/cv-esp.pdf"
         frameborder="0"
-        allowfullscreen="true"
       >
         Este navegador no puede mostrar el archivo PDF.
       </iframe>
